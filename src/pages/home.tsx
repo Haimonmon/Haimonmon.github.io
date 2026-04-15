3421f9d110bc2087fe4e79248c8aeb7b321fa163
Haimonmon <beankor8@gmail.com>
2026-04-15 15:57:50 +0800

    added useLayoutEffect for background change

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,14 @@
-import type { JSX } from "react";
+import { type JSX, useLayoutEffect } from "react";
 
 /**
  * Provides the full portfolio contents
  * @returns JSX.Element
  */
 const HomePage = (): JSX.Element => {
+    useLayoutEffect(() => {
+        document.body.className = "official-page";
+    }, []);
+
     return <h1>Official Page</h1>
 };
 

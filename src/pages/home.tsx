f0c1507294e02707f8a3ea347a102af2afb07ca2
Haimonmon <beankor8@gmail.com>
2026-04-15 14:10:06 +0800

    added all of the source file

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from "react";
+
+/**
+ * Provides the full portfolio contents
+ * @returns JSX.Element
+ */
+const HomePage = (): JSX.Element => {
+    return <h1>Official Page</h1>
+};
+
+export default HomePage;

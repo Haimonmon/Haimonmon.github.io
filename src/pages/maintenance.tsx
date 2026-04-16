d66ef87a2cf492a40299a6ac00599c893589b9f7
Haimonmon <beankor8@gmail.com>
2026-04-16 15:20:39 +0800

    content added and some links

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -1,12 +1,39 @@
-import { type JSX, useLayoutEffect } from "react";
+import { type JSX, type MouseEvent, useEffect } from "react";
 
-
-const MaintenancePage = (): JSX.Element => {
-    useLayoutEffect(() => {
+/**
+ * Provides the maintenance contents
+ * @returns JSX element
+ */
+export default function MaintenancePage(): JSX.Element {
+    useEffect(() => {
         document.body.className = 'maintenance-page';
     }, [])
 
-    return <h1>Maintenance Mode</h1>
-}
+    // * Redirect guest to my pdf resume
+    const goToResume = (): void => {
+        window.open("resume/DeCastroVinceCarlo.pdf", "_blank");
+    }
 
-export default MaintenancePage
+    // * Redirect guest to my github profile page
+    const goToGithub = (e: MouseEvent): void => {
+        e.stopPropagation()
+        window.open("https://github.com/Haimonmon", "_blank")
+    }
+
+    return (
+        <div className="clickable-area" onClick={goToResume}>
+            <div className="maintenance-container">
+                <div className="error-title-container">
+                    <h5>NOT ERROR</h5>
+                </div>
+                <p>Vince Portfolio is currently undergoing scheduled maintenance and is temporarily unavailable.</p>
+                <p id="stop-code">STOP_CODE: MAINTENANCE_MODE</p>
+                <ul>
+                    <li>* A Temporary resume has been provided as an alternative.</li>
+                    <li onClick={goToGithub} id="maintenance-github-link">* Access to the <span id="fake-anchor-github-link">Github</span> profile is available.</li>
+                </ul>
+                <p id="resume-message">Press any key to download resume<span id="underline-cursor">_</span></p>
+            </div>
+        </div>
+    )
+}

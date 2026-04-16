85b1e3ec72e35f3a807435eea0841b31014a9c12
Haimonmon <beankor8@gmail.com>
2026-04-16 17:13:43 +0800

    just changed into original function

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,12 +4,10 @@ import { type JSX, useEffect } from "react";
  * Provides the full portfolio contents
  * @returns JSX Element
  */
-const HomePage = (): JSX.Element => {
+export default function HomePage(): JSX.Element {
     useEffect(() => {
         document.body.className = "official-page";
     }, []);
 
     return <h1 id="testing-official">Official Page</h1>
 };
-
-export default HomePage;

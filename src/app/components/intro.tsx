975758bd249e1c6fb3ccbd1a1b333f6a37339280
Haimonmon <beankor8@gmail.com>
2026-04-20 14:37:08 +0800

    added intro animations and component

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -1,4 +1,4 @@
-import { type JSX } from 'react';
+import { useEffect, useState, type JSX } from 'react';
 
 
 let isShown: boolean = false;
@@ -8,7 +8,64 @@ let isShown: boolean = false;
  * @returns React jsx element
  */
 export default function Intro(): JSX.Element {
+    const message: string = "Hello World;"
+
+    const [isClosed, setClosed] = useState<boolean>(false);
+    const [index, setIndex] = useState<number>(0);
+    const [isTyping, setTyping] = useState<boolean>(false);
+    const [isCursorHidden, setCursorHidden] = useState<boolean>(false);
+
+    useEffect(() => {
+        let timeout: NodeJS.Timeout;
+
+        const type = (i = 0) => {
+            if (i > message.length) {
+                setTyping(false);
+                return;
+            }
+
+            setIndex(i)
+
+            timeout = setTimeout((): void => {
+                type(i + 1);
+            }, Math.random() * 50 + 45);
+        }
+
+        // * 1. Fast Typing effect
+        setTimeout((): void => {
+            setTyping(true);
+            type();
+        }, 900)
+
+        // * 2. Enter effect making the cursor dissappear
+        setTimeout((): void => setCursorHidden(true), 2900)
+        
+        // * 3. Closing the curtain container from what i called lmao.
+        setTimeout((): void => setClosed(true), 3500)
+
+        return () => clearTimeout(timeout)
+    }, []);
+
     return (
-        <h1></h1>
+        <div className={`${isClosed ? '[clip-path:inset(0_0_100%_0)]' : '[clip-path:inset(0_0_0_0)]'} transition-[clip-path] duration-2000 ease-in-out w-full h-full top-0 bg-dark-intro absolute visible justify-center flex`}>
+            <p className='text-[1.3rem]  text-white-coded font-[Fira_Code] mt-52 font-extralight'>
+                {message.split("").map((char, i) => {
+                    if (i >= index) {
+                        return null;
+                    };
+
+                    if (isCursorHidden) console.log("Entered!")
+
+                    const isHello: boolean = i < 5;
+
+                    return (
+                        <span key={i} className={`${isHello ? "text-blue-coded" : "text-yellow-coded"}${isCursorHidden ? "" : "/65"}`}>
+                            {char}
+                        </span>
+                    );
+                })}
+                <span className={`${isTyping ? '' : 'animate-blinker'} text-purple-coded ${isCursorHidden ? 'hidden' : ''}`}>_</span>
+            </p>
+        </div>
     )
 }

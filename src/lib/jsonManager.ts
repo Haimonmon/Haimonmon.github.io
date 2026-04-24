05e71230c05340ca570a98578b9931d7f2be2b2f
Haimonmon <beankor8@gmail.com>
2026-04-25 00:35:18 +0800

    gonna use this soon for my data jsons

---
+++ src/lib/jsonManager.ts
@@ -0,0 +1,25 @@
+/**
+ * Handles json files super easy on my portfolio
+ */
+class JsonManager {
+
+    /**
+     * 
+     * @param data - data to add to the json file.
+     * @param path - link relative path to save the data.
+     * 
+     */
+    save(data: any, path: string): void {
+
+    }
+
+    /**
+     * 
+     * @param path - link to relative path to retrieve the json data.
+     */
+    load(path: string) {
+
+    }
+}
+
+export default JsonManager;

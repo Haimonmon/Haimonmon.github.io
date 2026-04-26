acc5ad071799c4e63ba46c9aca3042d1fb4d7eb1
Haimonmon <beankor8@gmail.com>
2026-04-26 22:51:43 +0800

    added a route for 404 page

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,16 +2,20 @@ import { lazy } from "react";
 import SPA from "./core/spa";
 
 const HomePage = lazy(() => import("./pages/home"));
-const AboutMePage = lazy(() => import("./pages/aboutme"));
-const ProjectsPage = lazy(() => import("./pages/projects"));
 const ReachMePage = lazy(() => import("./pages/reach"));
+const AboutMePage = lazy(() => import("./pages/aboutme"));
+const UnknownPage = lazy(() => import("./pages/unknown"));
+const ProjectsPage = lazy(() => import("./pages/projects"));
 
 const app: SPA = new SPA();
+const isStrict: boolean = false; // * For Debugging purposes only
 
 app.add('/', HomePage);
 app.add("connect", ReachMePage);
 app.add("about-me", AboutMePage);
 app.add("projects", ProjectsPage);
 
-app.render();
+// * For 404 Not Found
+app.add("*", UnknownPage);
 
+app.render(isStrict);

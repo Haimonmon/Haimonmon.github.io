095cd682c6b2407ea46b8e34882413a53b826889
Haimonmon <beankor8@gmail.com>
2026-04-26 22:49:30 +0800

    added a page for 404 page

---
+++ src/app/pages/unknown.tsx
@@ -0,0 +1,12 @@
+import { type JSX } from 'react';
+import { useLocation } from 'react-router-dom'
+
+export default function UnknownPage(): JSX.Element {
+    const unknownPageName: string = useLocation().pathname
+
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <h1 className='text-white-coded font-bold text-2xl'>Well i dont know {unknownPageName} Nigger?</h1>
+        </div>  
+    )
+}

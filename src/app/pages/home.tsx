c41d850374ae0ba1e0a0ba811f8cdecd26c26e6a
Haimonmon <beankor8@gmail.com>
2026-04-26 22:49:55 +0800

    removed intro since the previous bug that only home will have an intro

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,29 +1,12 @@
-import { type JSX, useEffect, useState } from "react";
-import Intro from "../components/intro";
-import Navigation from "../components/navigation";
+import { type JSX } from "react";
 
 /**
  * Provides the full portfolio contents
  * @returns JSX Element
  */
 export default function HomePage(): JSX.Element {
-    const [isIntroShown, setIntroShown] = useState<boolean>(() => !sessionStorage.getItem('seenIntro'));
-
-    useEffect(() => {
-        if (!isIntroShown) return
-
-        const timeout = setTimeout(() => {
-            sessionStorage.setItem('seenIntro', "true")
-            setIntroShown(false);
-            console.log("Intro is now off.")
-        }, 4900);
-
-        return () => clearTimeout(timeout);
-    }, []);
-
     return (
         <>
-            {/* {isIntroShown ? <Intro message="Hello World;" disable={false} /> : ''} */}
             <h1 className="font-bold text-white-coded text-[78px]" onClick={():void => console.log("owo")}>Official Page</h1>
             <h1 className="font-bold text-white-coded text-[78px]" onClick={():void => console.log("owo")}>Official Page</h1>
             <h1 className="font-bold text-white-coded text-[78px]" onClick={():void => console.log("owo")}>Official Page</h1>

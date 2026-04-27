2cf1d2d369a72e2431f152bb0622e5ee0d2fae1c
Haimonmon <beankor8@gmail.com>
2026-04-27 11:52:43 +0800

    addded tailwind code for parent container sizing on responsiveness

--- src/utils/maintenanceRoutes.tsx
+++ src/utils/maintenanceRoutes.tsx
@@ -1,7 +1,6 @@
 import { type JSX, lazy,  useEffect, useState } from 'react';
 import { Outlet, useLocation, matchPath, type Location } from 'react-router-dom';
 
-// source code splitting: https://youtu.be/JU6sl_yyZqs?si=_eytUmg11QDG7KeD
 import Intro from "@components/intro";
 const Navigation = lazy(() => import("@components/navigation"));
 
@@ -13,7 +12,7 @@ const MaintenancePage = lazy(() => import("../app/pages/maintenance"));
 export default function MaintenanceRoute(props: {routes: {path: string, element: JSX.Element}[]}): JSX.Element {
     const location: Location = useLocation()
     const isMaintenanced: boolean = import.meta.env.VITE_APP_MAINTENANCE_MODE === "true" || false;
-    const isNotFound = !props.routes.filter((route) => route.path !== "*").some((route) => matchPath(route.path, location.pathname))
+    const isNotFound: boolean = !props.routes.filter((route) => route.path !== "*").some((route) => matchPath(route.path, location.pathname))
 
     const [isIntroShown, setIntroShown] = useState<boolean>(() => !sessionStorage.getItem('seenIntro'));
     
@@ -32,18 +31,27 @@ export default function MaintenanceRoute(props: {routes: {path: string, element:
         return () => clearTimeout(timeout);
     }, []);
 
-    // * Planning to have different 404 Pages between maintenance and not maintenance
-    if (isNotFound) return (
-        <Outlet/>
-    )
+    // // * Planning to have different 404 Pages between maintenance and not maintenance
+    // if (isNotFound) return (
+    //     <Outlet/>
+    // )
 
     return !isMaintenanced 
     ? (
-        <>
+        <div className='
+            wrap-anywhere
+            w-full
+            ml-2 mr-2
+            max-w-109.5
+            pablet:max-w-md
+            large-pablet:ml-[1.5%] large-pablet:mr-[1.5%] 
+            large-pablet:max-w-2xl
+            tablet:max-w-[46.4rem] 
+            small-desktop:max-w-[56.87rem]'>
             {isIntroShown ? <Intro message="Hello World;" disable={false} /> : ''}
             <Navigation/>
             <Outlet/>
-        </> 
+        </div> 
     )
-    : <MaintenancePage/>
+    : <MaintenancePage isNotFound={isNotFound}/>
 }

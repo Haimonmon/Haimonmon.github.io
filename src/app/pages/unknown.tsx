71e6903f36e2694c8b60e1aabf582be6033b67c0
Haimonmon <beankor8@gmail.com>
2026-04-27 11:52:11 +0800

    working on this

--- src/app/pages/unknown.tsx
+++ src/app/pages/unknown.tsx
@@ -5,7 +5,7 @@ export default function UnknownPage(): JSX.Element {
     const unknownPageName: string = useLocation().pathname
 
     return (
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="h-full flex justify-center items-center">
             <h1 className='text-white-coded font-bold text-2xl'>Well i dont know {unknownPageName} Nigger?</h1>
         </div>  
     )

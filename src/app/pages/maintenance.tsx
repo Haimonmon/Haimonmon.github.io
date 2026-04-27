067d8b00225ce60e44dfeccdcd9a274f924b9883
Haimonmon <beankor8@gmail.com>
2026-04-27 11:51:54 +0800

    working on 404 not found

--- src/app/pages/maintenance.tsx
+++ src/app/pages/maintenance.tsx
@@ -4,7 +4,7 @@ import { type JSX, type MouseEvent, useEffect, useState } from "react";
  * Provides the maintenance contents
  * @returns JSX element
  */
-export default function MaintenancePage(): JSX.Element {
+export default function MaintenancePage(props: {isNotFound: boolean}): JSX.Element {
     const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 900);
 
     // * Checks if the device of the guest is desktop or mobile 
@@ -63,7 +63,7 @@ export default function MaintenancePage(): JSX.Element {
         <div className="h-full flex justify-center tablet:w-full" onClick={goToResumeMobile}>
             <div className="w-[81.25vw] mt-[40vw] mb-[20vw] tablet:mb-[2vw] tablet:w-[64.844vw] tablet:h-[36.719vw] tablet:mt-[20vw] small-desktop:w-[67.578vw] small-desktop:h-[21.68vw] small-desktop:mt-[10vw] large-desktop:w-[48.058vw] large-desktop:h-[15.417vw] large-desktop:mt-[10vw]">
                 <div className="small-desktop:mb-[2.93vw] flex items-center justify-center mb-[9.375vw] tablet:mb-[3.906vw] large-desktop:mb-[2.083vw]">
-                    <h5 className="large-desktop:text-[1.389vw] large-desktop:w-[8.958vw] large-desktop:h-[2.083vw] small-desktop:text-[1.953vw] small-desktop:w-[12.598vw] small-desktop:h-[2.93vw] flex justify-center items-center font-[BSOD_Font] text-[5vw] h-[6.875vw] w-[32.813vw] text-blue-bsod bg-white-bsod tablet:text-[2.604vw] tablet:w-[16.797vw] tablet:h-[3.906vw] font-bold">NOT ERROR</h5>
+                    <h5 className="large-desktop:text-[1.389vw] small-desktop:text-[1.953vw] flex justify-center items-center font-[BSOD_Font] text-[5vw] h-fit w-fit text-blue-bsod bg-white-bsod tablet:text-[2.604vw] font-bold pr-3 pl-3 pt-[0.563rem] pb-2">NOT ERROR</h5>
                 </div>
                 <p className="large-desktop:text-[1.25vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-justify text-white-bsod text-[4.375vw] tablet:text-[2.344vw]">Vince Portfolio is currently undergoing scheduled maintenance and is temporarily unavailable.</p>
                 <p id="stop-code" className="large-desktop:text-[1.25vw] small-desktop:mb-[1.465vw] small-desktop:mt-[1.465vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mt-[3.125vw] mb-[4.688vw] tablet:text-[2.344vw] tablet:mt-[1.953vw] tablet:mb-[1.953vw] large-desktop:mt-[1.042vw] large-desktop:mb-[1.042vw]">STOP_CODE: MAINTENANCE_MODE</p>

f3c1773a8cd0ceebc594e048fc5e22bbda822676
Haimonmon <beankor8@gmail.com>
2026-06-28 21:43:00 +0800

    added todo

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,24 @@
-import path from 'path';
-import { defineConfig, type UserConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
+import path from "path";
+import { defineConfig, type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const viteConfig: UserConfig = defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
+    plugins: [react(), tailwindcss()],
     server: {
-        port: 9000
+        port: 9000,
     },
     resolve: {
+        // TODO: Fix this as its very confusing to have differnt aliases
         alias: {
             "@utils": path.resolve(__dirname, "./src/utils"),
             "@library": path.resolve(__dirname, "./src/lib"),
             "@data-pages": path.resolve(__dirname, "./src/data/pages"),
             "@components": path.resolve(__dirname, "./src/app/components"),
             "@data-components": path.resolve(__dirname, "./src/data/components"),
-            "@data": path.resolve(__dirname, "./src/data")
-        }
-    }
-})
+            "@data": path.resolve(__dirname, "./src/data"),
+        },
+    },
+});
 
-export default viteConfig
+export default viteConfig;

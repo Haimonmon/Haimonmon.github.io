d9fbb9193a8f634d04235a2462bc0ba6b4011685
Haimonmon <beankor8@gmail.com>
2026-06-28 21:44:21 +0800

    prettier doing its job

--- src/app/pages/maintenance.tsx
+++ src/app/pages/maintenance.tsx
@@ -1,96 +1,133 @@
 import { type JSX, type MouseEvent, useEffect, useState } from "react";
-import maintenanceData from '@data-pages/maintenance.json';
+import maintenanceData from "@data-pages/maintenance.json";
+import my from "@data/me.json";
+import type { Socials } from "../typings/global.types";
 
 type MaintenanceData = {
     [key: string]: {
-        errorTitle: string,
-        errorMessage: string,
-        errorType: string,
+        errorTitle: string;
+        errorMessage: string;
+        errorType: string;
         instructions: {
-            messages: string[],
+            messages: string[];
             interaction: {
-                message: string,
-                path: string
-            }
-        }
-    }
-}
+                message: string;
+                path: string;
+            };
+        };
+    };
+};
 
 /**
  * Provides the maintenance contents
  * @returns JSX element
  */
-export default function MaintenancePage(props: {isNotFound: boolean}): JSX.Element {
+export default function MaintenancePage(props: { isNotFound: boolean }): JSX.Element {
     const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth <= 900);
-    
-    const typedMaintenanceData = maintenanceData satisfies MaintenanceData;
 
-    // * Checks if the device of the guest is desktop or mobile 
+    const typedMaintenanceData = maintenanceData as MaintenanceData;
+
+    // * Checks if the device of the guest is desktop or mobile
     useEffect(() => {
-        document.body.classList.remove('bg-dark-coded')
-        document.body.classList.add('bg-blue-bsod')
+        document.body.classList.remove("bg-dark-coded");
+        document.body.classList.add("bg-blue-bsod");
 
         const handleWindowSizeChange = (): void => {
-            setIsMobile(window.innerWidth <= 900)
-        }
+            setIsMobile(window.innerWidth <= 900);
+        };
 
         console.log(window.innerWidth);
 
-        window.addEventListener('resize', handleWindowSizeChange)
+        window.addEventListener("resize", handleWindowSizeChange);
 
-        return () => { window.removeEventListener('resize', handleWindowSizeChange)}
-    }, [])
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+        };
+    }, []);
 
     // * Allows for
     useEffect(() => {
         const handleAllKeyDown = (): void => {
             goToResumeDesktop();
-        }
+        };
 
-        window.addEventListener('keydown', handleAllKeyDown);
+        window.addEventListener("keydown", handleAllKeyDown);
 
-        return () => { window.removeEventListener('keydown', handleAllKeyDown)}
-    }, [])
+        return () => {
+            window.removeEventListener("keydown", handleAllKeyDown);
+        };
+    }, []);
 
     useEffect(() => {
-        document.body.classList.add(isMobile ? 'mobile': 'desktop')
-    }, [isMobile])
+        document.body.classList.add(isMobile ? "mobile" : "desktop");
+    }, [isMobile]);
 
     // * Redirect guest to my pdf resume for mobile
     const goToResumeMobile = (): void => {
         // console.log("IS MOBILE?: ", isMobile)
-        if (!isMobile) return
+        if (!isMobile) return;
 
-        window.open("/resume/DeCastroVinceCarlo.pdf", "_blank");
-    }
+        window.open(my.resume, "_blank");
+    };
 
     // * Redirect guest to my pdf resume for desktop
     const goToResumeDesktop = (): void => {
-        if (isMobile) return
+        if (isMobile) return;
 
-        window.open("/resume/DeCastroVinceCarlo.pdf", "_blank");
-    }
+        window.open(my.resume, "_blank");
+    };
 
     // * Redirect guest to my github profile page
     const goToGithub = (e: MouseEvent): void => {
-        e.stopPropagation()
-        window.open("https://github.com/Haimonmon", "_blank")
-    }
+        const socials = my?.socials as Socials[];
+
+        e.stopPropagation();
+
+        window.open(socials[0]?.url, "_blank");
+    };
 
     return (
         <div className="h-full flex justify-center tablet:w-full" onClick={goToResumeMobile}>
             <div className="w-[81.25vw] mt-[40vw] mb-[20vw] tablet:mb-[2vw] tablet:w-[64.844vw] tablet:h-[36.719vw] tablet:mt-[20vw] small-desktop:w-[67.578vw] small-desktop:h-[21.68vw] small-desktop:mt-[10vw] large-desktop:w-[48.058vw] large-desktop:h-[15.417vw] large-desktop:mt-[10vw]">
                 <div className="small-desktop:mb-[2.93vw] flex items-center justify-center mb-[9.375vw] tablet:mb-[3.906vw] large-desktop:mb-[2.083vw]">
-                    <h5 className="large-desktop:text-[1.389vw] small-desktop:text-[1.953vw] flex justify-center items-center font-[BSOD_Font] text-[5vw] h-fit w-fit text-blue-bsod bg-white-bsod tablet:text-[2.604vw] font-bold pr-3 pl-3 pt-[0.563rem] pb-2">NOT ERROR</h5>
+                    <h5 className="large-desktop:text-[1.389vw] small-desktop:text-[1.953vw] flex justify-center items-center font-[BSOD_Font] text-[5vw] h-fit w-fit text-blue-bsod bg-white-bsod tablet:text-[2.604vw] font-bold pr-3 pl-3 pt-[0.563rem] pb-2">
+                        NOT ERROR
+                    </h5>
                 </div>
-                <p className="large-desktop:text-[1.25vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-justify text-white-bsod text-[4.375vw] tablet:text-[2.344vw]">Vince Portfolio is currently undergoing scheduled maintenance and is temporarily unavailable.</p>
-                <p id="stop-code" className="large-desktop:text-[1.25vw] small-desktop:mb-[1.465vw] small-desktop:mt-[1.465vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mt-[3.125vw] mb-[4.688vw] tablet:text-[2.344vw] tablet:mt-[1.953vw] tablet:mb-[1.953vw] large-desktop:mt-[1.042vw] large-desktop:mb-[1.042vw]">STOP_CODE: MAINTENANCE_MODE</p>
+                <p className="large-desktop:text-[1.25vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-justify text-white-bsod text-[4.375vw] tablet:text-[2.344vw]">
+                    Vince Portfolio is currently undergoing scheduled maintenance and is temporarily
+                    unavailable.
+                </p>
+                <p
+                    id="stop-code"
+                    className="large-desktop:text-[1.25vw] small-desktop:mb-[1.465vw] small-desktop:mt-[1.465vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mt-[3.125vw] mb-[4.688vw] tablet:text-[2.344vw] tablet:mt-[1.953vw] tablet:mb-[1.953vw] large-desktop:mt-[1.042vw] large-desktop:mb-[1.042vw]"
+                >
+                    STOP_CODE: MAINTENANCE_MODE
+                </p>
                 <ul className="large-desktop:mb-[3.125vw] small-desktop:mb-[2.441vw] mb-[7.813vw] tablet:mb-[3.255vw]">
-                    <li className="large-desktop:mb-[2vw] large-desktop:text-[1.25vw] small-desktop:mb-[2vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mb-[6.25vw] list-none tablet:text-[2.344vw] tablet:mb-[2vw]">* A Temporary resume has been provided as an alternative.</li>
-                    <li id="maintenance-github-link" className="large-desktop:mb-[2vw] large-desktop:text-[1.25vw] small-desktop:mb-[2vw] small-desktop:text-[1.758vw] tablet:mb-[2vw] tablet:text-[2.344vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mb-[6.25vw] list-none">* <span onClick={goToGithub} className="cursor-pointer">Access to the <span id="fake-anchor-github-link" className="underline">Github</span> profile is available.</span></li>
+                    <li className="large-desktop:mb-[2vw] large-desktop:text-[1.25vw] small-desktop:mb-[2vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mb-[6.25vw] list-none tablet:text-[2.344vw] tablet:mb-[2vw]">
+                        * A Temporary resume has been provided as an alternative.
+                    </li>
+                    <li
+                        id="maintenance-github-link"
+                        className="large-desktop:mb-[2vw] large-desktop:text-[1.25vw] small-desktop:mb-[2vw] small-desktop:text-[1.758vw] tablet:mb-[2vw] tablet:text-[2.344vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] mb-[6.25vw] list-none"
+                    >
+                        *{" "}
+                        <span onClick={goToGithub} className="cursor-pointer">
+                            Access to the{" "}
+                            <span id="fake-anchor-github-link" className="underline">
+                                Github
+                            </span>{" "}
+                            profile is available.
+                        </span>
+                    </li>
                 </ul>
-                <p className="large-desktop:text-[1.25vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] tablet:text-center tablet:text-[2.344vw]"><span onClick={goToResumeDesktop} className="cursor-pointer">Press any key to download resume<span className="animate-blinker">_</span></span></p>
+                <p className="large-desktop:text-[1.25vw] small-desktop:text-[1.758vw] font-[BSOD_Font] text-white-bsod text-[4.375vw] tablet:text-center tablet:text-[2.344vw]">
+                    <span onClick={goToResumeDesktop} className="cursor-pointer">
+                        Press any key to download resume<span className="animate-blinker">_</span>
+                    </span>
+                </p>
             </div>
         </div>
-    )
+    );
 }
